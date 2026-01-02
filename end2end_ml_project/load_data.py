b0a57a4024c70800460716fc3e5f4de45e1ebeba
rameshthiyagu <rameshthiyagu27@gmail.com>
2026-01-02 13:44:32 -0500

    train_test_split

--- end2end_ml_project/load_data.py
+++ end2end_ml_project/load_data.py
@@ -3,6 +3,8 @@ import pandas as pd
 import tarfile
 import urllib.request
 import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split
 
 def load_housing_data():
     tarball_path = Path("datasets/housing.tgz")
@@ -33,6 +35,25 @@ def hist(housing_data: any):
     housing_data.hist(bins=50, figsize=(12,8))
     plt.show()
 
+def shuffle_and_split_data(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    print(shuffled_indices)
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+def sklearn_shuffle_and_split_data(data, test_ratio):
+    return train_test_split(data, test_size=test_ratio, random_state=42)
+
+def data_volume(source, train, test):
+    print(f"Source: {len(source)}")
+    print(f"Train: {len(train)}")
+    print(f"Test: {len(test)}")
+
 housing_full = load_housing_data()
-standard_operating_procedure(housing_full)
-hist(housing_full)
+train_set, test_set = shuffle_and_split_data(housing_full, 0.2)
+#train_set, test_set = sklearn_shuffle_and_split_data(housing_full, 0.2)
+data_volume(housing_full, train_set, test_set)
+standard_operating_procedure(train_set)
+hist(train_set)
