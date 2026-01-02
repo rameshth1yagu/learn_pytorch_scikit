262b8cae86e313ef5ce3620dd185735754108cdc
rameshthiyagu <rameshthiyagu27@gmail.com>
2026-01-02 00:01:59 -0500

    sop and histogram

--- end2end_ml_project/load_data.py
+++ end2end_ml_project/load_data.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import pandas as pd
 import tarfile
 import urllib.request
+import matplotlib.pyplot as plt
 
 def load_housing_data():
     tarball_path = Path("datasets/housing.tgz")
@@ -13,10 +14,25 @@ def load_housing_data():
             housing_tarball.extractall(path="datasets")
     return pd.read_csv(Path("datasets/housing/housing.csv"))
 
+def standard_operating_procedure(housing_data: any):
+    # Get first 5 rows
+    print(housing_data.head())
+    # Print Info - Class, Row count, Column infos like name, non-null count, datatype
+    housing_data.info()
+    # Get Distinct value along with count for column 'ocean_proximity'
+    print(housing_data["ocean_proximity"].value_counts())
+    # Mathematical Statistics for numerical columns
+    print(housing_data.describe())
+
+def hist(housing_data: any):
+    plt.rc('font', size=14)
+    plt.rc('axes', labelsize=14, titlesize=14)
+    plt.rc('legend', fontsize=14)
+    plt.rc('xtick', labelsize=10)
+    plt.rc('ytick', labelsize=10)
+    housing_data.hist(bins=50, figsize=(12,8))
+    plt.show()
+
 housing_full = load_housing_data()
-# Get first 5 rows
-print(housing_full.head())
-# Print Info - Class, Row count, Column infos like name, non-null count, datatype
-housing_full.info()
-# Get Distinct value along with count for column 'ocean_proximity'
-print(housing_full["ocean_proximity"].value_counts())
+standard_operating_procedure(housing_full)
+hist(housing_full)
